972c9ffe1680a47931bc0bf8dbf3577b1a36d097
Nguyen Phan Minh Khai <nguyen.khai@kaonbroadband.com>
2024-11-18 14:32:43 +0700

    javascript functions

---
+++ function.js
@@ -0,0 +1,32 @@
+var makeNoise = function() {
+    console.log("Pling!");
+};
+
+// makeNoise();
+
+var power = function(base, exponent){
+    var result = 1;
+
+    for (var count = 0; count < exponent; ++count){
+        result *= base;
+    }
+    return result;
+};
+
+// console.log(power(2,10));
+
+var x = 'outside';
+
+var f1 = function(){
+    var x = 'inside f1';
+};
+
+f1();
+// console.log(x);
+
+var f2 = function(){
+    x = 'inside f2';
+};
+
+f2();
+console.log(x);
